e65cf8110dc4f18b76bacfb7d6ccf8d2f482df9c
Alexandre Ferreira <alex.jorge.m@gmail.com>
2018-06-09 23:36:47 +0100

    changes on cmake file

--- env.sh
+++ env.sh
@@ -1,16 +1,18 @@
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
-function c() { make clean; rm cmake_install.cmake; rm CMakeLists.txt.user; rm CMakeCache.txt; rm -rf CMakeFiles; rm -rf bin; }
+function c() { make clean; rm cmake_install.cmake; rm CMakeLists.txt.*; rm CMakeCache.txt; rm -rf CMakeFiles; rm -rf out; }
 function b() { cmake .; make -j8; }
 function bd() { cmake -DCMAKE_BUILD_TYPE=Debug .; make -j8; ldd bin/dbg/libdedicatedslave.so; readelf -d bin/dbg/libdedicatedslave.so; }
-function br() { b && sh -c 'cd bin && ./dedicatedslave-gui' }
-function r() { sh -c 'cd bin && ./dedicatedslave-gui'}
+function br() { b; r;}
+function r() { sh -c 'cd out/bin && ./dedicatedslave-gui'}
+function rc() { sh -c 'cd out/bin && ./dedicatedslave'}
 function p() { git add .; git commit -m "Script Deploy - `echo ${DATE}`"; git push; }
 
 echo "‘c’ - Clean the build files"
 echo "‘b’ - Build"
 echo "'bd' - Build w/ Debug flag"
 echo "‘br’ - Build and runs"
-echo "‘r’ - Runs"
+echo "‘r’ - Runs GUI"
+echo "‘rc’ - Runs CLI"
 echo "‘p’ - Push"
 
 # Windows Packaging
